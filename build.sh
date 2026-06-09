b68d090b8879cf43bc273cd9e3e41c45691a6e21
Kroese <kroese@users.noreply.github.com>
2026-06-09 19:00:01 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -508,6 +508,17 @@ export DEB_VERSION=$(dpkg-parsechangelog -SVersion)
 export DEB_VERSION_UPSTREAM=$(dpkg-parsechangelog -SVersion | cut -d- -f1)
 
 dpkg-buildpackage -a${HOST_ARCH} -b -us -uc ${BUILD_PROFILES}
+
+cd ui
+
+${SUDO} apt -y build-dep -a${HOST_ARCH} ${BUILD_PROFILES} .
+dpkg-buildpackage -a${HOST_ARCH} -b -us -uc ${BUILD_PROFILES}
+
+ls -lh
+exit 1
+
+cd ..
+
 cd ..
 
 shopt -s nullglob
